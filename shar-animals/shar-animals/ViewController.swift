998405e82cee0c92aba54bbd5ed3e178d0085104
Daniel <danielr9@utas.edu.au>
2016-08-28 11:35:51 +1000

    database management and location
    please keep committed version of info.plist because authorization has been added to it.

--- shar-animals/shar-animals/ViewController.swift
+++ shar-animals/shar-animals/ViewController.swift
@@ -1,25 +1,59 @@
 //
 //  ViewController.swift
-//  shar-animals
+//  sqltestbizzle
 //
-//  Created by Brendan Hodkinson on 27/08/2016.
-//  Copyright © 2016 Brendan Hodkinson. All rights reserved.
+//  Created by Daniel Richardson on 27/08/2016.
+//  Copyright © 2016 Daniel Richardson. All rights reserved.
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var latitude: UITextField!
+    @IBOutlet weak var longitude: UITextField!
+    @IBOutlet weak var comments: UITextField!
+    
+    let mysql = MySql();    // handles the mysql HTTP POST
+    /* handles getting user location of where the picture was taken */
+    let locManager = CLLocationManager()
+    var currentLocation = CLLocation()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locManager.requestWhenInUseAuthorization()
+        check_authorization()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /* A convienante way to check that the user has given authorization to
+     * use location services and inform them if they haven't authorized it but
+     * want to use the services */
+    func check_authorization() -> Bool {
+        
+        if( CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.Authorized) {
+            
+            let refreshAlert = UIAlertView()
+            refreshAlert.title = "Authorization denied"
+            refreshAlert.message = "Please check your settings"
+            refreshAlert.addButtonWithTitle("OK")
+            refreshAlert.show()
+            
+            return false
+        }
+        
+        return true
     }
-
-
+    
+    /* Sends the location data back to the model to add to the database */
+    @IBAction func send_to_db(sender: UIButton) {
+        if (check_authorization()) {
+            var latitude = "\(currentLocation.coordinate.latitude)"
+            var longitude = "\(currentLocation.coordinate.longitude)"
+            
+            mysql.query_sql(latitude, longitude: longitude, comments: comments.text!);
+        }
+    }
 }
 
