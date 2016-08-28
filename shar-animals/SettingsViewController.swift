a45c46e97ea3321957599efdf07e8b85376bb866
Brendan Hodkinson <bldh@utas.edu.au>
2016-08-28 16:20:59 +1000

    Polish, App icons, Location collecting, Database

---
+++ shar-animals/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  shar-animals
+//
+//  Created by Brendan Hodkinson on 28/08/2016.
+//  Copyright © 2016 Brendan Hodkinson. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+}
--- shar-animals/ViewController.swift
+++ shar-animals/ViewController.swift
@@ -7,24 +7,88 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var Camera: UIButton!
     @IBOutlet weak var Gallery: UIButton!
     @IBOutlet weak var ImageDisplay: UIImageView!
     
+    let mysql = MySql();    // handles the mysql HTTP POST
+    /* handles getting user location of where the picture was taken */
+    let locManager = CLLocationManager()
+    var currentLocation = CLLocation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locManager.requestWhenInUseAuthorization()
+        check_authorization()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func addImage(sender: AnyObject) {
+        var alertController:UIAlertController?
+        alertController = UIAlertController(title: "Collection Submission",
+                                            message: "Enter name of animal",
+                                            preferredStyle: .Alert)
+        
+        alertController!.addTextFieldWithConfigurationHandler(
+            {(textField: UITextField!) in
+                textField.placeholder = "Enter name of animal"
+        })
+        
+        let action = UIAlertAction(title: "Submit",
+                                   style: UIAlertActionStyle.Default,
+                                   handler: {[weak self]
+                                    (paramAction:UIAlertAction!) in
+                                    if let textFields = alertController?.textFields{
+                                        if (NSUserDefaults.standardUserDefaults().boolForKey("shareData")){
+                                            let textfields = textFields as [UITextField]
+                                            let enteredText = textFields[0].text
+                                            self!.send_to_db(enteredText!)
+                                        }
+                                    }
+            })
+        
+        alertController?.addAction(action)
+        self.presentViewController(alertController!,
+                                   animated: true,
+                                   completion: nil)
+    }
+    
+    func check_authorization() -> Bool {
+        
+        if( CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.Authorized) {
+            
 
+            
+            return true
+        }
+        
+        let refreshAlert = UIAlertView()
+        refreshAlert.title = "Authorization denied"
+        refreshAlert.message = "Please check your settings"
+        refreshAlert.addButtonWithTitle("OK")
+        refreshAlert.show()
+        
+        return false
+    }
+    
+    func send_to_db(comment: String) {
+        if (check_authorization()) {
+            var latitude = "\(currentLocation.coordinate.latitude)"
+            var longitude = "\(currentLocation.coordinate.longitude)"
+            
+            mysql.query_sql(latitude, longitude: longitude, comments: comment);
+        }
+    }
     
     @IBAction func GalleryLibraryAction(sender: UIButton)
     {
